8f141eec8b8262ac2a7aef7f5476580d1a1ec571
Raphael Vandon <r.vandon@criteo.com>
2013-10-19 15:54:03 +0200

    with background, follows mouse

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 var preloadCount = 0;
-var preloadTotal = 1;
+var preloadTotal = 2;
 
 var imgPlayer = new Image();
 var objPlayer;
+
+var imgBg = new Image();
+var imgBullet = new Image();
 
 var stage;
 
@@ -15,6 +18,9 @@ function preloadAssets()
 {
 	imgPlayer.onload = preloadUpdate();
 	imgPlayer.src = "media/joueur.png";
+
+	imgBg.onload = preloadUpdate();
+	imgBg.src = "media/bg.jpg";
 }
 
 function preloadUpdate()
@@ -27,26 +33,25 @@ function preloadUpdate()
 function launchGame()
 {
 	stage = new createjs.Stage(document.getElementById("gameCanvas"));
+
+	var objBg = new createjs.Bitmap(imgBg);
+	stage.addChild(objBg);
+
 	objPlayer = new createjs.Bitmap(imgPlayer);
 	objPlayer.regX = 64;
-	objPlayer.x = 64;
-
+	objPlayer.regY = 32;
+	objPlayer.x = 70;
+	objPlayer.y = 300;
 	stage.addChild(objPlayer);
 
 	createjs.Ticker.setFPS(30);
 	createjs.Ticker.addEventListener("tick", update);
 }
 
-var moveIncrement = 4;
+var speed = 4
 
 function update()
 {
-	objPlayer.x += moveIncrement;
-	if(objPlayer.x > 800 - 64 || objPlayer.x < 64)
-	{
-		moveIncrement = -(moveIncrement*1.2);
-		objPlayer.y += 64;
-		objPlayer.scaleX = -objPlayer.scaleX;
-	}
+	objPlayer.y = Math.min(Math.max(stage.mouseY, 32), 600-32);
 	stage.update();
 }
