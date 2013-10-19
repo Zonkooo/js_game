3470bcb91120c9f602b396860ddeaeef9e6d6fed
Raphael Vandon <r.vandon@criteo.com>
2013-10-19 14:41:46 +0200

    move left and right and flip

--- js/script.js
+++ js/script.js
@@ -1,9 +1,49 @@
+var preloadCount = 0;
+var preloadTotal = 1;
+
+var imgPlayer = new Image();
+var objPlayer;
+
+var stage;
+
 function startGame()
 {
-	var stage = new createjs.Stage(document.getElementById("gameCanvas"));
-	var text = new createjs.Text("CRITEO SAY LE BIEN", "36px Arial", "#777777");
-	stage.addChild(text);
-	text.x = 300;
-	text.y = 200;
+	preloadAssets();
+}
+
+function preloadAssets()
+{
+	imgPlayer.onload = preloadUpdate();
+	imgPlayer.src = "media/joueur.png";
+}
+
+function preloadUpdate()
+{
+	preloadCount++;
+	if(preloadCount == preloadTotal)
+		launchGame();
+}
+
+function launchGame()
+{
+	stage = new createjs.Stage(document.getElementById("gameCanvas"));
+	objPlayer = new createjs.Bitmap(imgPlayer);
+	stage.addChild(objPlayer);
+
+	createjs.Ticker.setFPS(30);
+	createjs.Ticker.addEventListener("tick", update);
+}
+
+var moveIncrement = 3;
+
+function update()
+{
+	objPlayer.x += moveIncrement;
+	if(objPlayer.x > 800 - 128)
+	{
+		moveIncrement = -(moveIncrement*1.2);
+		objPlayer.y += 64;
+		objPlayer.scaleX = -objPlayer.scaleY;
+	}
 	stage.update();
 }
