92c4b5fbafcaa1b18c85b2ffd971d4ad95a0c85f
Raphael Vandon <r.vandon@criteo.com>
2013-10-20 15:05:26 +0200

    collisions and specs

--- js/script.js
+++ js/script.js
@@ -13,6 +13,19 @@ var stage;
 var bullets = new Array();
 var enemies = new Array();
 
+var reloadSpeed = 80;
+var moveSpeed = 5;
+var enemySpawnSpeed = 75;
+var bulletSpeed = 6;
+
+function Enemy()
+{
+	this.offset = Math.random() * 10;
+	this.amplitude = Math.random() * 2 + 0.5;
+	this.freq = Math.random()*0.02;
+	this.tex = new createjs.Bitmap(imgEnemy);
+}
+
 function startGame()
 {
 	preloadAssets();
@@ -61,17 +74,25 @@ function launchGame()
 	stage.addEventListener("click", fire);
 }
 
-var enemyCountdown = 30;
+var enemyCountdown = 100;
 
 function update()
 {
-	//mouve player
-	objPlayer.y = Math.min(Math.max(stage.mouseY, 32), stage.canvas.height-32);
+	//move player
+	var nextY = Math.min(Math.max(stage.mouseY, 32), stage.canvas.height-32);
+	if(Math.abs(nextY - objPlayer.y) < moveSpeed)
+		objPlayer.y = nextY;
+	else if(nextY - objPlayer.y < 0)
+		objPlayer.y -= moveSpeed;
+	else
+		objPlayer.y += moveSpeed;
+
+
 
 	//move bullets forward
 	for(i = 0; i < bullets.length; i++)
 	{
-		bullets[i].x += 6;
+		bullets[i].x += bulletSpeed;
 		if(bullets[i].x > stage.canvas.width)
 		{
 			stage.removeChild(bullets[i]);
@@ -86,33 +107,75 @@ function update()
 	else
 	{
 		spawnEnemy();
-		enemyCountdown = 30;
+		enemyCountdown = enemySpawnSpeed + 20 * Math.random();
 	}
 
 	//move enemies
 	for(i = 0; i < enemies.length; i++)
 	{
-		enemies[i].x--;
-		enemies[i].y += Math.sin(enemies[i].x*0.08)*5;
+		var e = enemies[i];
+		e.tex.x--;
+		e.tex.y += Math.sin((e.offset + e.tex.x)*e.freq)*e.amplitude;
 	}
+
+	checkCollision();
+	reload--;
 
 	stage.update();
 }
 
+function checkCollision()
+{
+	for(i = 0; i < bullets.length; i++)
+		for(j = 0; j < enemies.length; j++)
+		{
+			var e = enemies[j].tex;
+			var b = bullets[i];
+
+			ex = e.x + 65;
+			ey = e.y + 61;
+			bx = b.x + 32;
+			by = b.y + 32;
+
+			var radius = 60;
+			if((ex-bx)*(ex-bx) + (ey-by)*(ey-by) < radius*radius)
+			{
+				bullets.splice(i, 1);
+				enemies.splice(j, 1);
+				stage.removeChild(b);
+				stage.removeChild(e);
+
+				reloadSpeed *= 0.95;
+				moveSpeed *= 1.05;
+				enemySpawnSpeed *= 0.95;
+				bulletSpeed *= 1.05;
+
+				i--;
+				break; //stop processing enemies for this bullet
+			}
+		}
+}
+
+var reload = 0;
+
 function fire()
 {
-	var nbBullets = bullets.length;
-	bullets[nbBullets] = new createjs.Bitmap(imgBullet);
-	bullets[nbBullets].x = objPlayer.x;
-	bullets[nbBullets].y = objPlayer.y;
-	stage.addChild(bullets[nbBullets]);
+	if(reload < 0)
+	{
+		var nbBullets = bullets.length;
+		bullets[nbBullets] = new createjs.Bitmap(imgBullet);
+		bullets[nbBullets].x = objPlayer.x;
+		bullets[nbBullets].y = objPlayer.y;
+		stage.addChild(bullets[nbBullets]);
+		reload = reloadSpeed;
+	}
 }
 
 function spawnEnemy()
 {
-	var enemy = new createjs.Bitmap(imgEnemy); //130x122
-	enemy.x = stage.canvas.width - 130;
-	enemy.y = (Math.random())*400;
-	stage.addChild(enemy);
+	var enemy = new Enemy(); //130x122
+	enemy.tex.x = stage.canvas.width;
+	enemy.tex.y = (Math.random())*400;
+	stage.addChild(enemy.tex);
 	enemies[enemies.length] = enemy;
 }
