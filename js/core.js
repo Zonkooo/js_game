e2eb8e7d93db395e1fa3f9827fa8334741817bbc
Raphael Vandon <r.vandon@criteo.com>
2014-12-14 17:17:57 +0100

    split fat code in separate files

--- js/core.js
+++ js/core.js
@@ -1,43 +1,5 @@
 var isKeyPressed = [];
 
-function Player(initX, initY, bitmap)
-{
-	var self = this; //to use in events
-
-	this.speed = 8;
-	this.jumpingPower = 15;
-
-	this.internal = bitmap;
-	this.internal.x = initX;
-	this.internal.y = initY;
-
-	this.verticalVelocity = 0;
-
-	this.Update = function()
-	{
-		if(this.internal.y == 360 && isKeyPressed[38])
-			this.verticalVelocity = -this.jumpingPower;
-		this.internal.y += this.verticalVelocity;
-
-		var move = 0
-		if(isKeyPressed[37])
-			move += -this.speed;
-		if(isKeyPressed[39])
-			move += this.speed;
-		this.internal.x += move;
-
-		//handle collision with the ground
-		if(this.internal.y >= 360 && this.verticalVelocity >= 0)
-		{
-			this.internal.y = 360;
-			this.verticalVelocity = 0;
-		}
-
-		if(this.internal.y != 360) //apply gravity only if airborne
-			this.verticalVelocity += 1;
-	}
-}
-
 var preloadCount = 0;
 var preloadTotal = 2;
 
